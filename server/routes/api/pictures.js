bc769a4622d2734cee90182c24472799edfee3e7
Marko Lojanica <marko.lojanica1991@gmail.com>
2020-10-13 13:36:48 +0200

    -fixed mime type for gif

--- server/routes/api/pictures.js
+++ server/routes/api/pictures.js
@@ -10,6 +10,7 @@ const MIME_TYPE_MAP = {
 						"image/png": "png",
 						"image/jpeg": "jpg",
 						"image/jpg": "jpg",
+						"image/gif" : "gif"
 					};
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
